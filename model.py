65f3e2aff5f3b8de6bdde0a3af62e57e1eea1982
stirelshka8 <stirelshka8@gmail.com>
2022-10-26 14:09:13 +0300

    Модель БД готова!

--- model.py
+++ model.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy import Column, Integer, String, ForeignKey, Float, Date
 from sqlalchemy.orm import declarative_base, relationship
 
 Base = declarative_base()
@@ -22,3 +22,39 @@ class Book(Base):
     title = Column(String(length=40), unique=True)
 
     publish = relationship(Publisher, backref='boo')
+
+
+class Shop(Base):
+    __tablename__ = 'shop'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(length=40), unique=True)
+
+
+class Stock(Base):
+    __tablename__ = 'stock'
+
+    id = Column(Integer, primary_key=True)
+    id_book = Column(Integer, ForeignKey('book.id'), nullable=False)
+    id_shop = Column(Integer, ForeignKey('shop.id'), nullable=False)
+    count = Column(Integer)
+
+    boo = relationship(Book, backref='stoc')
+    sho = relationship(Shop, backref='stoc')
+
+
+class Sale(Base):
+    __tablename__ = 'sale'
+
+    id = Column(Integer, primary_key=True)
+    id_stock = Column(Integer, ForeignKey('stock.id'), nullable=False)
+    price = Column(Float, nullable=False)
+    date_sale = Column(Date, nullable=False)
+    count = Column(Integer)
+
+    stoc = relationship(Stock, backref='sal')
+
+
+def tables_create(engine):
+    Base.metadata.drop_all(engine)
+    Base.metadata.create_all(engine)
